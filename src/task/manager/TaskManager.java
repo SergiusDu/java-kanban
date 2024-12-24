169d65f64f13baca5950ae0120342accd60daa03
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2024-12-24 13:05:16 -0500

    refactor(TaskManager, TaskRepository): enhance task handling
    - Updated `TaskManager` to support type-safe addition of different task types (`RegularTask`, `EpicTask`, `SubTask`) using `TypeSafeCaster`.
    - Modified `TaskRepository` methods (`addTask`, `removeTaskById`) to return `Optional` for safer null handling.
    - Renamed parameters in several methods of `TaskRepository` for better clarity (`filter` to `taskPredicate`).
    - Improved JavaDoc comments across `TaskRepository` to provide more precise and detailed descriptions.
    - Maintained backward compatibility while improving overall code readability and safety.

--- src/task/manager/TaskManager.java
+++ src/task/manager/TaskManager.java
@@ -4,41 +4,56 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import task.model.EpicTask;
+import task.model.RegularTask;
+import task.model.SubTask;
 import task.model.Task;
 import task.store.TaskRepository;
+import util.TypeSafeCaster;
 
 public class TaskManager {
-    private final TaskRepository store;
-    public TaskManager(final TaskRepository store) {
-        this.store = store;
-    }
+  private final TaskRepository store;
 
-    public Collection<Task> getAllTasks() {
-        return store.getAllTasks();
-    }
+  public TaskManager(final TaskRepository store) {
+    this.store = store;
+  }
 
-    public void clearAllTasks() {
-        store.clearAllTasks();
-    }
+  public Collection<Task> getAllTasks() {
+    return store.getAllTasks();
+  }
 
-    public Optional<Task> getTaskById(int id) {
-        return store.getTaskById(id);
-    }
+  public void clearAllTasks() {
+    store.clearAllTasks();
+  }
 
-    public Task addTask(final Task task) {
-        return store.addTask(task.getId(), task);
-    }
+  public Optional<Task> getTaskById(int id) {
+    return store.getTaskById(id);
+  }
 
-    public Task updateTask(final Task task) {
-        return store.updateTask(task.getId(), task);
-    }
+  public Optional<RegularTask> addTask(final RegularTask task) {
+    return Optional.ofNullable(
+        TypeSafeCaster.castSafely(store.addTask(task.getId(), task), RegularTask.class));
+  }
 
-    public Collection<Task> getEpicSubtasks(EpicTask epicTask) {
-        List<Integer> subtasksIds = epicTask.getSubtaskIds();
-        return store.findTasksMatching(task -> subtasksIds.contains(task.getId()));
-    }
+  public Optional<EpicTask> addTask(final EpicTask task) {
+    return Optional.ofNullable(
+        TypeSafeCaster.castSafely(store.addTask(task.getId(), task), EpicTask.class));
+  }
 
-    public Collection<Task> getAllTasksByClass(Class<Task> targetClass) {
-        return store.findTasksMatching(targetClass::isInstance).stream().toList();
-    }
+  public Optional<SubTask> addTask(final SubTask task) {
+    return Optional.ofNullable(
+        TypeSafeCaster.castSafely(store.addTask(task.getId(), task), SubTask.class));
+  }
+
+  public Task updateTask(final Task task) {
+    return store.updateTask(task.getId(), task);
+  }
+
+  public Collection<Task> getEpicSubtasks(EpicTask epicTask) {
+    List<Integer> subtasksIds = epicTask.getSubtaskIds();
+    return store.findTasksMatching(task -> subtasksIds.contains(task.getId()));
+  }
+
+  public Collection<Task> getAllTasksByClass(Class<Task> targetClass) {
+    return store.findTasksMatching(targetClass::isInstance).stream().toList();
+  }
 }
